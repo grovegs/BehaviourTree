6316b00ab3edde0ddbc134bf48a5428a4c03ba9a
GencerG <guzeloglugencer@gmail.com>
2024-11-01 13:56:41 +0300

    feat: add godot variant blackboard

---
+++ src/GroveGames.BehaviourTree.Godot/VariantBlackboard.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Godot;
+
+using GroveGames.BehaviourTree.Collections;
+
+namespace GroveGames.BehaviourTree;
+
+public class VariantBlackboard : IBlackboard
+{
+    private readonly Dictionary<string, Variant> _dataBase;
+
+    public VariantBlackboard()
+    {
+        _dataBase = [];
+    }
+
+    public void DeleteValue(string key)
+    {
+        _dataBase.Remove(key);
+    }
+
+    public T? GetValue<T>(string key)
+    {
+        return _dataBase.TryGetValue(key, out var value) ? value.As<T>() : default(T);
+    }
+
+    public void SetValue<T>(string key, T obj) where T : notnull
+    {
+        _dataBase[key] = Variant.From(obj);
+    }
+}

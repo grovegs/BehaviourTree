b63e0e1f80353f144166186c977f21cadb27a9d8
GencerG <guzeloglugencer@gmail.com>
2024-11-08 11:07:18 +0300

    feat: add enable/disable feature

--- src/GroveGames.BehaviourTree/Tree.cs
+++ src/GroveGames.BehaviourTree/Tree.cs
@@ -4,19 +4,25 @@ namespace GroveGames.BehaviourTree;
 
 public abstract class Tree
 {
+    private bool _isEnabled;
     private readonly Root _root;
-
     protected Root Root => _root;
 
     public Tree(Root root)
     {
         _root = root;
+        _isEnabled = false;
     }
 
     public abstract void SetupTree();
 
     public virtual void Tick(float deltaTime)
     {
+        if (!_isEnabled)
+        {
+            return;
+        }
+
         _root.Evaluate(deltaTime);
     }
 
@@ -28,5 +34,15 @@ public abstract class Tree
     public virtual void Abort()
     {
         _root.Abort();
+    }
+
+    public void Enable()
+    {
+        _isEnabled = true;
+    }
+
+    public void Disable()
+    {
+        _isEnabled = false;
     }
 }

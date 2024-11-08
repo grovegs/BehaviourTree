bad4c80b329cd8036631e60af71af6705ae4a126
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-11-08 12:42:26 +0300

    test: move delayer test to decorators

--- tests/GroveGames.BehaviourTree.Tests/Nodes/Decorators/DelayerTests.cs
+++ tests/GroveGames.BehaviourTree.Tests/Nodes/Decorators/DelayerTests.cs
@@ -1,12 +1,12 @@
 using GroveGames.BehaviourTree.Nodes;
 using GroveGames.BehaviourTree.Nodes.Decorators;
 
-namespace GroveGames.BehaviourTree.Tests.Nodes.Composites;
+namespace GroveGames.BehaviourTree.Tests.Nodes.Decorators;
 
 public class DelayerTests
 {
     [Fact]
-    public void Delayer_ReturnsRunning_WhenIntervalIsLessThanWaitTime()
+    public void Evaluate_ShouldReturnRunningWhenIntervalIsLessThanWaitTime()
     {
         // Arrange
         float waitTime = 2.0f;
@@ -14,36 +14,14 @@ public class DelayerTests
         var delayer = new Delayer(mockParent.Object, waitTime);
 
         // Act
-        var result = delayer.Evaluate(1.0f); // First tick with a deltaTime less than the wait time
+        var result = delayer.Evaluate(1.0f);
 
         // Assert
         Assert.Equal(NodeState.Running, result);
     }
 
     [Fact]
-    public void Delayer_ExecutesChild_WhenIntervalReachesWaitTime()
-    {
-        // Arrange
-        float waitTime = 2.0f;
-        var mockParent = new Mock<IParent>();
-        var mockChild = new Mock<INode>();
-        var delayer = new Delayer(mockParent.Object, waitTime);
-        delayer.Attach(mockChild.Object); // Attach the child node to the delayer
-
-        mockChild.Setup(child => child.Evaluate(It.IsAny<float>())).Returns(NodeState.Success); // Set up the child node to return SUCCESS when evaluated
-
-        // Act
-        var firstTickResult = delayer.Evaluate(1.0f); // First tick (still running as wait time has not been reached)
-        var secondTickResult = delayer.Evaluate(1.0f); // Second tick (reaches wait time)
-
-        // Assert
-        Assert.Equal(NodeState.Running, firstTickResult);
-        Assert.Equal(NodeState.Success, secondTickResult);
-        mockChild.Verify(child => child.Evaluate(It.IsAny<float>()), Times.Once); // Child should be evaluated once
-    }
-
-    [Fact]
-    public void Delayer_ResetsInterval_WhenChildCompletes()
+    public void Evaluate_ShouldExecuteChildWhenIntervalReachesWaitTime()
     {
         // Arrange
         float waitTime = 2.0f;
@@ -55,16 +33,17 @@ public class DelayerTests
         mockChild.Setup(child => child.Evaluate(It.IsAny<float>())).Returns(NodeState.Success);
 
         // Act
-        delayer.Evaluate(1.0f); // First tick (still running as wait time has not been reached)
-        delayer.Evaluate(1.0f); // Second tick (reaches wait time, evaluates child, and should reset interval)
-        var thirdTickResult = delayer.Evaluate(0.5f); // Next tick should be running as the interval has reset
+        var firstTickResult = delayer.Evaluate(1.0f);
+        var secondTickResult = delayer.Evaluate(1.0f);
 
         // Assert
-        Assert.Equal(NodeState.Running, thirdTickResult);
+        Assert.Equal(NodeState.Running, firstTickResult);
+        Assert.Equal(NodeState.Success, secondTickResult);
+        mockChild.Verify(child => child.Evaluate(It.IsAny<float>()), Times.Once);
     }
 
     [Fact]
-    public void Delayer_Abort_ResetsInterval()
+    public void Evaluate_ShouldResetIntervalWhenChildCompletes()
     {
         // Arrange
         float waitTime = 2.0f;
@@ -73,10 +52,31 @@ public class DelayerTests
         var delayer = new Delayer(mockParent.Object, waitTime);
         delayer.Attach(mockChild.Object);
 
-        delayer.Evaluate(1.5f); // Increment the interval by evaluating once
+        mockChild.Setup(child => child.Evaluate(It.IsAny<float>())).Returns(NodeState.Success);
 
         // Act
-        delayer.Abort(); // Call Abort and check if interval has reset
+        delayer.Evaluate(1.0f);
+        delayer.Evaluate(1.0f);
+        var thirdTickResult = delayer.Evaluate(0.5f);
+
+        // Assert
+        Assert.Equal(NodeState.Running, thirdTickResult);
+    }
+
+    [Fact]
+    public void Abort_ShouldResetIntervalWhenAbortIsCalled()
+    {
+        // Arrange
+        float waitTime = 2.0f;
+        var mockParent = new Mock<IParent>();
+        var mockChild = new Mock<INode>();
+        var delayer = new Delayer(mockParent.Object, waitTime);
+        delayer.Attach(mockChild.Object);
+
+        delayer.Evaluate(1.5f);
+
+        // Act
+        delayer.Abort();
         var resultAfterAbort = delayer.Evaluate(0.5f);
 
         // Assert
@@ -85,7 +85,7 @@ public class DelayerTests
     }
 
     [Fact]
-    public void Delayer_Reset_ResetsInterval()
+    public void Reset_ShouldResetIntervalWhenResetIsCalled()
     {
         // Arrange
         float waitTime = 2.0f;
@@ -94,10 +94,10 @@ public class DelayerTests
         var delayer = new Delayer(mockParent.Object, waitTime);
         delayer.Attach(mockChild.Object);
 
-        delayer.Evaluate(1.5f); // Increment the interval by evaluating once
+        delayer.Evaluate(1.5f);
 
         // Act
-        delayer.Reset(); // Call Reset and check if interval has reset
+        delayer.Reset();
         var resultAfterReset = delayer.Evaluate(0.5f);
 
         // Assert

226042133f828278f7f870468f513e4d6a8b09d9
Gencer Güzeloğlu <59722791+GencerG@users.noreply.github.com>
2024-12-26 14:10:31 +0300

    fix: implement missing members

--- src/GroveGames.BehaviourTree/Nodes/Root.cs
+++ src/GroveGames.BehaviourTree/Nodes/Root.cs
@@ -48,4 +48,14 @@ public sealed class Root : IRoot
         tree.SetupTree(this);
         return this;
     }
+
+    public void OnEnter()
+    {
+
+    }
+
+    public void OnExit()
+    {
+
+    }
 }

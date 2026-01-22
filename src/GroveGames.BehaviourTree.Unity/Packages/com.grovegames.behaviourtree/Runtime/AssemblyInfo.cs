26bd4042425e48c5c7be379a5f897dbfddd3dbbb
Onur Tanrıkulu <onurtnrkl@gmail.com>
2026-01-22 19:44:30 +0300

    refactor: move unity package to submodule

--- src/GroveGames.BehaviourTree.Unity/Packages/com.grovegames.behaviourtree/Runtime/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GroveGames.BehaviourTree.Unity.Editor")]
-[assembly: InternalsVisibleTo("GroveGames.BehaviourTree.Unity.Tests")]

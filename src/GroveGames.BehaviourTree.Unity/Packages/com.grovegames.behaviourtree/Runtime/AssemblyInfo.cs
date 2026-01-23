e377164ee0e999debcadc0740a88be58e65322bc
Onur Tanrıkulu <onurtnrkl@gmail.com>
2026-01-23 15:45:08 +0300

    Add Unity package for GroveGames BehaviourTree
    Initial commit of the GroveGames.BehaviourTree Unity package, including runtime, editor, and test assembly definitions, AssemblyInfo files, and package metadata. This sets up the structure for modular and extensible behavior tree development in Unity.

---
+++ src/GroveGames.BehaviourTree.Unity/Packages/com.grovegames.behaviourtree/Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("GroveGames.BehaviourTree.Unity.Editor")]
+[assembly: InternalsVisibleTo("GroveGames.BehaviourTree.Unity.Tests")]
